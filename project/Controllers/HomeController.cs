f99c36151b53e99d13102f238473635265b200ee
Abdo Mohammad <Abdo_Mohammad@DARALEFTA.LOCAL>
2023-05-31 11:55:02 +0300

    version1

---
+++ project/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Book()
+        {
+            return View();
+        }
+        public ActionResult Index_Book()
+        {
+            return View();
+        }
+        public ActionResult magazine()
+        {
+            return View();
+        }
+        public ActionResult search()
+        {
+            return View();
+        }
+        public ActionResult Number_Page()
+        {
+            return View();
+        }
+
+    }
+}
